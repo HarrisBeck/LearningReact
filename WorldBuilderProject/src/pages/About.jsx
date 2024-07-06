0f59d79c0c393c35e6735cdd2fd303e44fc9cc41
Harris Beck <65682433+HarrisBeck@users.noreply.github.com>
2024-07-06 01:17:57 -0300

    Current About message

--- WorldBuilderProject/src/pages/About.jsx
+++ WorldBuilderProject/src/pages/About.jsx
@@ -2,7 +2,7 @@ function About()
 {
     return (
         <p>
-            Welcome to the happenings page.
+            This site has been created to act as a tool for my homebrew TTRPG campaign settings. It is meant to be interacted with by players in order to enrich their experience in the world and is meant to be added to by the developer in order to keep notes and build the world.
         </p>
     );
 }
