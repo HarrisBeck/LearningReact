0647fb7a793d8b62cf7cfbe795ad7f049bbd0c7f
Harris Beck <65682433+HarrisBeck@users.noreply.github.com>
2024-07-11 23:17:26 -0300

    Beginning making the first newspaper

--- WorldBuilderProject/src/pages/Happenings.jsx
+++ WorldBuilderProject/src/pages/Happenings.jsx
@@ -2,12 +2,22 @@ function Happenings() {
   return (
     <div className="Newspaper">
       <div className="NewspaperHead">
-        <h1 className="NewspaperHeadFont">Eskainian Word</h1>
+        <h1 className="NewspaperHeadFont">Eskain Enquire</h1>
       </div>
-      <p>
-        This is a test to make sure that the body is the correct level below the
-        actual text
-      </p>
+      <div className="container mt-3">
+        <div className="row">
+          <div className = "col column">
+            <p>
+              This is a test
+            </p>
+          </div>
+          <div className = "col-3 column">
+            <p>
+              Of the separation of the different aspects.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
