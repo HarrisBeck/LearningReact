afbd1672a8bcc9df9d893600424963a8a298fb59
Harris Beck <65682433+HarrisBeck@users.noreply.github.com>
2024-07-11 23:06:49 -0300

    Simple fixes for newspaper

--- WorldBuilderProject/src/pages/Happenings.jsx
+++ WorldBuilderProject/src/pages/Happenings.jsx
@@ -1,13 +1,12 @@
 function Happenings() {
   return (
-    <div className="NewspaperBackground">
+    <div className="Newspaper">
       <div className="NewspaperHead">
-        <h1 className="NewspaperHeadFont">
-          Eskainian Word
-        </h1>
+        <h1 className="NewspaperHeadFont">Eskainian Word</h1>
       </div>
       <p>
-        This is a test to make sure that the body is the correct level below the actual text
+        This is a test to make sure that the body is the correct level below the
+        actual text
       </p>
     </div>
   );
